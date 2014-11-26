3c24754d2084410e3b61cf6e751d1200c6967d44
Juan Carlos <Juan@Hasimov>
2014-11-26 21:21:43 +0100

    añado Ej01

---
+++ src/ejerciciostema1/Ej01.java
@@ -0,0 +1,11 @@
+package ejerciciostema1;
+
+public class Ej01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Esto es una prueba");
+	}
+
+}
